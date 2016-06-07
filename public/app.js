810d8271f050b2866a873191869a74cc7f29f0f1
Donald Lessells <donald.lessells@gmail.com>
2016-06-07 21:25:23 +0100

    tidied up a bit

--- public/app.js
+++ public/app.js
@@ -1,8 +1,4 @@
 var lastCountry = JSON.parse(localStorage.getItem('last country')) || [];
-console.log(lastCountry.name);
-console.log(lastCountry.length);
-
-
 
 
 window.onload = function() {
@@ -10,69 +6,59 @@ window.onload = function() {
   var url = "https://restcountries.eu/rest/v1";
   var request = new XMLHttpRequest();
 
-  console.log(request);
   request.open("GET", url);
-  request.onload = function() {//this will only run when we have the api data
-    if (request.status === 200) { //i.e. if request successful
+  request.onload = function() {
+    if (request.status === 200) {
       console.log('got the data');
       var jsonString = request.responseText;
       countries = JSON.parse(jsonString);
-      var country = countries[0];
-      console.log(country.name);
-      console.log(country.population);
-      console.log(country.capital);
+     
       createForm(countries);
 
       var select = document.getElementById('country-select');
       select.onchange = changeEventHandler;
 
-    }
-
-
-    
+      if (lastCountry) {
+        showCountryData(lastCountry);
+      }
+    }    
   }
-
-  request.send(null); // need this to send the request. null as we're not
-                      // sending any data. if the http method above were e.g
-                      // 'put' instead of 'get' then we might have data in the
-                      // arguments
-
-  if (lastCountry) {
-    var h3 = document.createElement('h3');
-    h3.id = 'selected-country-name';
-    var body = document.getElementsByTagName('body')[0];
-    var list = document.createElement('ul');
-    list.id = 'unnecessary-list';
-    h3.innerText = lastCountry.name;
-    var li1 = document.createElement('li');
-    var p1 = document.createElement('p');
-    var p2 = document.createElement('p');
-    var li2 = document.createElement('li');
-    var p3 = document.createElement('p');
-    var p4 = document.createElement('p');
-    p1.innerText = 'Capital';
-    p2.innerText = lastCountry.capital;
-    p3.innerText = 'Population';
-    p4.innerText = lastCountry.population;
-    body.appendChild(list);
-    list.appendChild(h3);
-    list.appendChild(li1);
-    li1.appendChild(p1);
-    li1.appendChild(p2);
-    list.appendChild(li2);
-    li2.appendChild(p3);
-    li2.appendChild(p4);
-  }
-
+  request.send(null);
 }
 
 
+var showCountryData = function(country) {
+
+  var h3 = document.createElement('h3');
+  h3.id = 'selected-country-name';
+  var body = document.getElementsByTagName('body')[0];
+  var list = document.createElement('ul');
+  list.id = 'unnecessary-list';
+  h3.innerText = country.name;
+  var li1 = document.createElement('li');
+  var p1 = document.createElement('p');
+  var p2 = document.createElement('p');
+  var li2 = document.createElement('li');
+  var p3 = document.createElement('p');
+  var p4 = document.createElement('p');
+  p1.innerText = 'Capital';
+  p2.innerText = country.capital;
+  p3.innerText = 'Population';
+  p4.innerText = country.population;
+  body.appendChild(list);
+  list.appendChild(h3);
+  list.appendChild(li1);
+  li1.appendChild(p1);
+  li1.appendChild(p2);
+  list.appendChild(li2);
+  li2.appendChild(p3);
+  li2.appendChild(p4);
+}
 
 
 var changeEventHandler = function(e) {
 
   if (document.getElementById('selected-country-name')) {
-
     var body = document.getElementsByTagName('body')[0];
     var list = document.getElementById('unnecessary-list');
     body.removeChild(list);
@@ -80,38 +66,12 @@ var changeEventHandler = function(e) {
 
   for (country of countries) {
     if (country.name === e.target.value) {
-      var h3 = document.createElement('h3');
-      h3.id = 'selected-country-name';
-      var body = document.getElementsByTagName('body')[0];
-      var list = document.createElement('ul');
-      list.id = 'unnecessary-list';
-      h3.innerText = country.name;
-      var li1 = document.createElement('li');
-      var p1 = document.createElement('p');
-      var p2 = document.createElement('p');
-      var li2 = document.createElement('li');
-      var p3 = document.createElement('p');
-      var p4 = document.createElement('p');
-      p1.innerText = 'Capital';
-      p2.innerText = country.capital;
-      p3.innerText = 'Population';
-      p4.innerText = country.population;
-      body.appendChild(list);
-      list.appendChild(h3);
-      list.appendChild(li1);
-      li1.appendChild(p1);
-      li1.appendChild(p2);
-      list.appendChild(li2);
-      li2.appendChild(p3);
-      li2.appendChild(p4);
-
+      showCountryData(country);
       locallyStore(country);
-
-    }
-  
+    }  
+  }
 }
 
-}
 
 var locallyStore = function(country) {
 
@@ -119,8 +79,6 @@ var locallyStore = function(country) {
   lastCountry.push(country);
   localStorage.setItem('last country', JSON.stringify(country));
 }
-
-
 
 
 var createForm = function(countries) {
